da7eb5f68fd108fa5586e5dd403ae8687229ee23
Ian Kluft <github-dev@iankluft.com>
2022-02-06 23:19:50 -0800

    add POD docs to PrefVote::Core::Set

--- src/perl/prefvote/lib/PrefVote/Core/Set.pm
+++ src/perl/prefvote/lib/PrefVote/Core/Set.pm
@@ -89,6 +89,7 @@ __PACKAGE__->make_immutable;
 __END__
 
 # POD documentation
+=encoding utf8
 
 =head1 NAME
 
@@ -96,11 +97,49 @@ PrefVote::Core::Set - Set data type which extends Types::Standard
 
 =head1 SYNOPSIS
 
+In class containing a set:
+
+    package Some::Classname;
+    use Moo;
+    use MooX::TypeTiny;
+    use Set::Tiny qw(set);
+    use PrefVote::Core::Set qw(Set);
+
+    has name => (
+        is => 'ro',
+        isa => Set[NonEmptySimpleStr],
+    );
+
+In code using Some::Classname's name set from above:
+
+    sub new_names { return Some::Classname->new(name => set(@_); }
+
+    my $abc_set = new_names(qw(a b c d e f));
+    if ($abc_set->name()->is_empty()) {
+        say "empty set";
+        exit 0;
+    }
+    say "elements: ".join(" ", $self->name()->elements());
 
 =head1 DESCRIPTION
 
+PrefVote::Core::Set is a set data type based on L<Set::Tiny>, which provides an unordered list of unique strings.
+It is compatible with L<Standard::Types> and the L<Moo> object system.
+If used as the name "Set" it is a set of strings.
+
+It is a parameterized type so that it can be a set of any scalar string type.
+So "Set[NonEmptySimpleStr]" in the synopsis is a set of strings which are constrained to be non-empty.
+
+Data elements are of type L<Set::Tiny>. So it is used for access to the data.
+
+The PrefVote system uses this for lists of candidates when they should be unordered, such as before votes are counted
+or within results among a group of candidates who are tied.
 
 =head1 SEE ALSO
+
+L<PrefVote>, L<Set::Tiny>, L<Moo>, L<Standard::Types>
+
+L<https://github.com/ikluft/prefvote>
 
 =head1 BUGS AND LIMITATIONS
 
