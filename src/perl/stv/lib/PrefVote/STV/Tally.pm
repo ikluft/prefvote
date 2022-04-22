207437c602677cccba214fb0563c04d622eaa3d5
Ian Kluft <github-dev@iankluft.com>
2022-04-22 16:55:23 -0700

    finish POD docs of PrefVote::STV::Tally

--- src/perl/stv/lib/PrefVote/STV/Tally.pm
+++ src/perl/stv/lib/PrefVote/STV/Tally.pm
@@ -124,11 +124,9 @@ sub mark_as_winner
     my ($self, %opts) = @_;
     #$self->debug_print("mark_as_winner(".$self->{name}."): opts = ".join(" ", %opts));
     $self->winner(1);
-    my @keys_set;
     foreach my $key (qw(place votes surplus transfer)) {
         if (exists $opts{$key}) {
             $self->$key($opts{$key});
-            push @keys_set, $key;
         }
     }
     return;
@@ -168,11 +166,20 @@ PrefVote::STV::Tally - internal per-round candidate tally structure used by Pref
 
 =head1 SYNOPSIS
 
+  my $stv_tally_ref = PrefVote::STV::Tally->new(name => 'test1');
+  my $stv_tally_ref2 = PrefVote::STV::Tally->new(name => 'test2');
+  $stv_tally_ref->mark_as_winner(place => 1, votes => 42, surplus => 12, transfer => 10);
+  my $votes1 = $stv_tally_ref->votes(); # 42
+  $stv_tally_ref2->mark_as_eliminated();
+  my $votes2 = $stv_tally_ref2->votes(); # 0
 
 =head1 DESCRIPTION
 
 ⛔ This is for PrefVote internal use only.
 
+I<PrefVote::STV::Tally> holds the per-round voting tally for a candidate.
+Each of these objects is held in a hash called 'tally' in L<PrefVote::STV::Round>,
+keyed on the candidate ID string, to store that vote-counting round's totals.
 
 =head1 ATTRIBUTES
 
@@ -180,17 +187,47 @@ PrefVote::STV::Tally - internal per-round candidate tally structure used by Pref
 
 =item name
 
+'Name' is the candidate ID string. It should match the key used to store it in L<PrefVote::STV::Round>'s
+tally hash attribute. It can also be used to look up data about the candidate from L<PrefVote::STV>
+and its parent class <PrefVote::Core>.
+
 =item votes
+
+'Votes' is the total votes received in the current round by the candidate.
+It may differ from totals for the same candididate in earlier or later rounds, depending on votes
+that transfer to voters' next choices when candidates are eliminated in any round.
 
 =item winner
 
+Winner is a boolean flag indicating the candidate is a winner of the current round.
+In case of ties, more than one candidate may have this flag set.
+
 =item eliminated
+
+'Eliminated' is a boolean flag indicating the candidate was eliminated in the current round.
+This happens in Single Transferable Vote counting when there is no winner of the round,
+meaning that no candidate(s) reached the necessary fraction (quota) of votes to win.
+Therefore the last place candidate, or all candidates tied for last place, are eliminated.
+Eliminated candidates have their votes redistributed to voters' next-place choices.
 
 =item place
 
+'Place' is an integer number indicating the nth place that a winning candidate of the round qualified for.
+Note that winning a round does not necessarily indicate winning an available seat in an election.
+If available seats were already awarded in earlier rounds, then this only indicates the placement in the results.
+
 =item transfer
 
+'Transfer' is the fraction of votes that a round's winning candidate is above the quota necessary to win.
+This fraction of votes is available to be transferred to voters' next choices in the same amount as the
+candidate is above the quota.
+In following rounds, votes may transfer in much smaller quantities as multiple transfer fractions are applied.
+
 =item surplus
+
+'Surplus' is the total of votes that a round's winning candidate is above the quota necessary to win.
+This number of votes is available to be transferred to voters' next choices in the same fraction as the
+amount above the quota, and used to compute the transfer fraction.
 
 =back
 
@@ -198,11 +235,21 @@ PrefVote::STV::Tally - internal per-round candidate tally structure used by Pref
 
 =over 1
 
-=item add_votes
+=item add_votes ( float )
 
-=item mark_as_winner
+This is used in vote-counting, after applying possibly multiple transfer fractions, to add a total to
+a candidate's tally for a round.
+
+=item mark_as_winner ( place => int, votes => float, surplus => float, transfer => float )
+
+This marks a candidate as a winner by setting the winner attribute to true.
+It can also apply a place, vote total and transfer/surplus fractions to the candidate.
+Parameters are given as key/value pairs. They may be given in any order, and will be used
+to assign the attributes of the object.
 
 =item mark_as_eliminated
+
+This marks a candidate as eliminated in the current round by setting the eliminated attribute to true.
 
 =back
 
