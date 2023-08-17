374285b87d8579554d6df20233f41d975d8f22bb
Ian Kluft <ko6yq.dev+github@gmail.com>
2023-08-16 17:45:30 -0700

    CEF parser rewrite (WIP): manual bump/updates to CEF_Parser.pm, otherwise no Parse::Yapp dependency

---
+++ src/perl/prefvote/tools/CEF_Parser-update.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+# CEF_Parser-update.pl - use Parse::Yapp to update CEF_Parser.pm from CEF_Parser.yp & CEF_Parser-template
+# Copyright (c) 2023 by Ian Kluft
+# Open Source license: Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
+#
+# Run this to update the generated parser code after modifications to the CEF grammar.
+use strict;
+use warnings;
+use utf8;
+use autodie;
+use feature qw(say);
+use v5.10.0;
+use Carp qw(croak);
+use Parse::Yapp;
+use Readonly;
+use FindBin        qw($Bin);
+use File::Basename qw(dirname);
+use File::Slurp    qw(read_file);
+
+# constants
+Readonly::Scalar my $TOOLS_PATH    => $Bin;
+Readonly::Scalar my $SOURCE_ROOT   => dirname($TOOLS_PATH);
+Readonly::Scalar my $UPDATE_PATH   => $SOURCE_ROOT . "/lib/PrefVote/Core/Input/";
+Readonly::Scalar my $CLASS_PREFIX  => "PrefVote::Core::Input::";
+Readonly::Scalar my $CEF_BASE      => "CEF_Parser";
+Readonly::Scalar my $CEF_GRAMMAR   => $UPDATE_PATH . "/" . $CEF_BASE . ".yp";
+Readonly::Scalar my $PM_OUT_PATH   => $UPDATE_PATH . "/" . $CEF_BASE . ".pm";
+Readonly::Scalar my $PACKAGE       => $CLASS_PREFIX . $CEF_BASE;
+Readonly::Scalar my $TEMPLATE_PATH => $TOOLS_PATH . "/CEF_Parser-template";
+Readonly::Hash my %REQUIRED_FILES => (
+    $CEF_GRAMMAR   => "CEF grammar input",
+    $TEMPLATE_PATH => "Parse::Yapp template",
+);
+
+# verify we found files to work with
+my @missing;
+foreach my $req_file ( keys %REQUIRED_FILES ) {
+    if ( not -f $req_file ) {
+        push @missing, $req_file;
+    }
+}
+if (@missing) {
+    say STDERR "missing required files - expected paths based on script location:";
+    foreach my $file (@missing) {
+        say STDERR "   " . $REQUIRED_FILES{$file} . " " . $file;
+    }
+    exit 1;
+}
+
+# run Parse::Yapp to build CEF parser
+my $template_text = read_file($TEMPLATE_PATH);
+my $parser        = Parse::Yapp->new( inputfile => $CEF_GRAMMAR );
+open my $out, ">", $PM_OUT_PATH
+    or croak "cannot open $PM_OUT_PATH for writing: $!";
+print $out $parser->Output(
+    classname   => $PACKAGE,
+    standalone  => 1,
+    linenumbers => 1,
+    template    => $template_text,
+);
+close $out
+    or croak "cannot close $PM_OUT_PATH: $!";
+
